462f10435fde808a8a8447563f482626e3b8081c
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-10-13 16:32:38 +0300

    Добавьте файлы проекта.

---
+++ Prey-Predator/main.cpp
@@ -0,0 +1,45 @@
+﻿// Prey-Predator.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+void start() {};
+void load() {};
+
+int main() {
+
+	int command;
+
+	cout << "--------Prey - predator model--------" << endl
+		<< "Pres '1' to start symulation" << endl
+		<< "Pres '2' to load existing symulation" << endl
+		<< "Pres '3' to exit program" << endl;
+
+	while (1) {
+		cout << "Enter command:" << endl;
+		cin >> command;
+		switch (command)
+		{
+		case 1: {
+			start();
+		}
+		case 2: {
+			load();
+		}
+		case 3: {
+			cout << "Close app" << endl;
+			exit(3);
+		}
+		default: {
+			cout << "No such command. Try one more time" << endl;
+			cin.clear();
+			cin.sync();
+			break;
+		}
+		}
+	}
+
+	return 0;
+}
+
