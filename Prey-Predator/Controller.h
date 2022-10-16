bb01bfe2c41daa3f4a80c8449928f715b586def2
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-10-16 18:10:18 +0300

    add class Controller and some methods for it

---
+++ Prey-Predator/Controller.h
@@ -0,0 +1,37 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+#include <vector>
+using namespace std;
+
+class Controller {
+
+private:
+	vector<Rabbit> vecRabbit;		// Приватні змінні для масивів вовків та зайців, мапи 
+	vector<Wolf> vecWolf;
+	Map gameMap;
+	// Також треба продумати від яких коефіцієнтів буде залежати симуляція
+	// голод, старість і т.д
+	// Скласти відповідні рівняння
+
+
+public:
+
+	void setVecRabbit(vector<Rabbit> vecRabbit); // Сетери для приватних змінних
+	void setVecWolf(vector<Wolf> vecWolf);
+	void setMap(Map gameMap);
+
+	vector<Rabbit> getVecRabbit(); // Гетери для приватних змінних
+	vector<Wolf> getVecWolf();
+	Map getMap();
+
+	void start();	//Старт симуляції, введення початкових параметрів
+
+	void refresh(); //Оновлення карти після кожного ходу
+
+	void insertToFile(); //Функції для запису та зчитування даних з файлу
+	void getFromFile();
+};
+
+
+
+#endif // !CONTROLLER_H
