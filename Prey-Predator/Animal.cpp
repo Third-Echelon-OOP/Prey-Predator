b910bc9863a0102e1057ec85194d199ce2e0b945
Kolia <chopyk.mykola@lll.kpi.ua>
2022-10-21 17:28:54 +0300

    добавил класс Animal

---
+++ Prey-Predator/Animal.cpp
@@ -0,0 +1,5 @@
+//
+// Created by PC1 on 21.10.2022.
+//
+
+#include "Animal.h"
---
+++ Prey-Predator/Animal.h
@@ -0,0 +1,14 @@
+//
+// Created by PC1 on 21.10.2022.
+//
+
+#ifndef PREY_PREDATOR_ANIMAL_H
+#define PREY_PREDATOR_ANIMAL_H
+
+
+class Animal {
+
+};
+
+
+#endif //PREY_PREDATOR_ANIMAL_H
