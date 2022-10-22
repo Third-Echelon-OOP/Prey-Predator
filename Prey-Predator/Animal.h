61e8e812abeb0a442ceda819e6d892cb427f6c03
Kolia <chopyk.mykola@lll.kpi.ua>
2022-10-22 22:49:47 +0300

    добавил стандартные геттеры и сеттеры, для класса от которого будут наследоваться классы хищников и добычи

--- Prey-Predator/Animal.h
+++ Prey-Predator/Animal.h
@@ -5,9 +5,28 @@
 #ifndef PREY_PREDATOR_ANIMAL_H
 #define PREY_PREDATOR_ANIMAL_H
 
-
+struct Coordinates{
+    int x;
+    int y;
+};
 class Animal {
-
+private:
+    bool alive;
+    Coordinates position;
+    static num_of_animals;
+public:
+    Animal();
+    Animal(int x, int y);
+    virtual ~Animal();
+    void set_x(int x);
+    void set_y(int y);
+    void live();
+    void death();
+    int get_x();
+    int get_y();
+    bool get_alive();
+    int get_num_of_animals();
+    double Distance_between_objects(int x, int y);
 };
 
 
