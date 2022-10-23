79fb5751d6e91c3ebfc4d03bbae94fe9b10cf04d
dima_samborskyi <samborskiydmitriy1@gmail.com>
2022-10-23 16:36:21 +0300

    changes in controller

--- Prey-Predator/Controller.cpp
+++ Prey-Predator/Controller.cpp
@@ -4,60 +4,8 @@ using namespace std;
 
 void Controller::start() {
 
-    Map gameMap;
+   
 
-    do    // Введення розмірів карти для симуляції
-    {
-        cout << "Enter length of map: ";
-        cin >> gameMap.length;
-
-        cout << "Enter width of field: ";
-        cin >> gameMap.width;
-
-        if ((gameMap.length < 100) && (gameMap.width < 100))
-        {
-            cout << "Too small map for 2 animals" << endl;
-        }
-    } while ((gameMap.length < 100) && (gameMap.width < 100)); // Мінімальні розміри карти можна змінити
-
-
-    /*cout << "Enter time which wolf can survive without food: ";   // Введення коефіцієнтів для симуляції
-    cin >> ;
-
-    cout << "Enter time needed for new rabbit to be born: ";
-    cin >> ;
-
-    cout << "Enter number of rabbits needed for new wolf to be born: ";
-    cin >> ;*/
-
-
-    //vector<Rabbit> vecRabbit;   //Створення вектора зайців
-
-    //unsigned int xR = rand() % gameMap.length + 1;
-    //unsigned int yR = rand() % gameMap.width + 1;
-
-    //Rabbit firstRabbit(xR, yR); // Конструктор зайця
-
-    //vecRabbit.push_back(firstRabbit);
-
-    //vector<Wolf> vecWolf; // Сворення вектора вовків
-
-    //unsigned int xW = rand() % gameMap.length + 1;
-    //unsigned int yW = rand() % gameMap.width + 1;
-
-    //while ((xR == xW) && (yR == yW))
-    //{
-    //    xW = rand() % gameMap.length + 1;
-    //    yW = rand() % gameMap.width + 1;
-    //}
-
-    //Wolf firstWolf(xW, yW); // Конструктор зайця
-
-    //vecWolf.push_back(firstWolf);
-
-    //Controller::setVecRabbit(vecRabbit);
-    //Controller::setVecRabbit(vecWolf);
-    //Controller::setMap(gameMap);
 }
 
 void Controller::refresh() {
--- Prey-Predator/Controller.h
+++ Prey-Predator/Controller.h
@@ -1,51 +1,22 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 #include <vector>
+#include "Animal.h"
 using namespace std;
 
-class Wolf{
-public:
-	int xW;
-	int yW;
-};
-class Rabbit {
-public:
-	int xR;
-	int yR;
-};
-class Map{
-public:
-	int length;
-	int width;
-};
 
 class Controller {
 
 private:
-	vector<Rabbit> vecRabbit;		// Приватні змінні для масивів вовків та зайців, мапи 
-	vector<Wolf> vecWolf;
 	Map gameMap;
-	// Також треба продумати від яких коефіцієнтів буде залежати симуляція
-	// голод, старість і т.д
-	// Скласти відповідні рівняння
-
+	Animal animalArray[100];
 
 public:
-	// Сетери для приватних змінних
-	void setVecRabbit(vector<Rabbit> Rabbits);
-	void setVecWolf(vector<Wolf> Wolfs);
-	void setMap(Map map);
 
-	// Гетери для приватних змінних
-	vector<Rabbit> getVecRabbit();
-	vector<Wolf> getVecWolf();
-	Map getMap();
 
-	void start();	//Старт симуляції, введення початкових параметрів
-
-	void refresh(); //Оновлення карти після кожного ходу
-
-	void insertToFile(); //Функції для запису та зчитування даних з файлу
+	void start();	
+	void refresh(); 
+	void insertToFile(); 
 	void getFromFile();
 };
 
